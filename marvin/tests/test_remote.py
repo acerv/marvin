4ba68ad4f99f1955bed3b21a78f13cfbced8d4a3
Andrea Cervesato <andrea.cervesato@mailbox.org>
2017-03-12 18:25:50 +0100

    more precise remote module unittest

--- marvin/tests/test_remote.py
+++ marvin/tests/test_remote.py
@@ -13,9 +13,15 @@ import shutil
 import unittest
 from marvin.remote import OpenSSH
 from marvin.remote import OpenSSHProtocol
+from marvin.remote import Serial
+from marvin.remote import SerialProtocol
 from marvin.remote import SFTPProtocol
 from marvin.remote import DataItem
 from marvin.remote import CommandStream
+from marvin.errors import SSHConnectionError
+from marvin.errors import RemotePathNotExistError
+from marvin.errors import LocalPathNotExistError
+from marvin.errors import SerialConnectionError
 
 class TestRemote(unittest.TestCase):
     """ Test the remote module """
@@ -107,27 +113,47 @@ class TestRemote(unittest.TestCase):
 
         self.assertFalse(os.path.exists(self.remotedir))
 
+    def test_sftp_path_not_exist_error(self):
+        """ test transferring a path that does not exist """
+        protocol = SFTPProtocol(
+            "localhost", 22,
+            "", "",
+            2.0
+        )
+
+        ssh = OpenSSH(protocol)
+
+        with self.assertRaises(LocalPathNotExistError):
+            item = DataItem("this_src_does_not_exist", "", "remote")
+            ssh.sftp_transfer([item])
+
+        with self.assertRaises(RemotePathNotExistError):
+            item = DataItem("this_src_does_not_exist", "", "local")
+            ssh.sftp_transfer([item])
+
     def _test_execute_callback(self, command, stdout, stderr):
         self.assertIn(command, self.commands)
         self.assertIsNotNone(stdout)
         self.assertIsNotNone(stderr)
 
-    def _create_stream(self):
+    def _create_stream(self, commands, results):
         class StreamTester(CommandStream):
             """ CommandStream tester """
-            def __init__(self, tester):
+            def __init__(self, tester, commands, results):
                 self._tester = tester
+                self._commands = commands
+                self._results = results
 
             def cmd_executing(self, command):
-                self._tester.assertIn(command, self._tester.commands)
+                self._tester.assertIn(command, self._commands)
 
             def handle_stdout_line(self, line):
                 self._tester.assertIsNotNone(line)
 
             def cmd_completed(self, result):
-                self._tester.assertIn(result, ['0', '1'])
+                self._tester.assertIn(result, self._results)
 
-        stream = StreamTester(self)
+        stream = StreamTester(self, commands, results)
         return stream
 
     def test_ssh_execute(self):
@@ -140,5 +166,75 @@ class TestRemote(unittest.TestCase):
 
         ssh = OpenSSH(protocol)
 
-        stream = self._create_stream()
-        self.assertEqual(ssh.ssh_execute(self.commands, stream), [0, 1])
+        results = ["0", "1"]
+        stream = self._create_stream(self.commands, results)
+        self.assertEqual(ssh.ssh_execute(self.commands, stream), results)
+
+    def test_ssh_address_error(self):
+        """ test a SSH address wrong assignment """
+        protocol = OpenSSHProtocol(
+            "this_host_does_not_exist", 22,
+            "", "",
+            2.0
+        )
+
+        ssh = OpenSSH(protocol)
+        with self.assertRaises(SSHConnectionError):
+            ssh.ssh_execute("ls")
+
+    def test_ssh_port_error(self):
+        """ test a SSH port wrong assignment """
+        protocol = OpenSSHProtocol(
+            "localhost", 2200,
+            "", "",
+            2.0
+        )
+
+        ssh = OpenSSH(protocol)
+        with self.assertRaises(SSHConnectionError):
+            ssh.ssh_execute("ls")
+
+    def test_ssh_user_error(self):
+        """ test a SSH user wrong assignment """
+        protocol = OpenSSHProtocol(
+            "localhost", 22,
+            "this_user_does_not_exist", "",
+            2.0
+        )
+
+        ssh = OpenSSH(protocol)
+        with self.assertRaises(SSHConnectionError):
+            ssh.ssh_execute("ls")
+
+    def test_serial_execute(self):
+        """ test serial command execution """
+        protocol = SerialProtocol(
+            "loop",
+            91200,
+            'odd',
+            1,
+            5,
+            1
+        )
+
+        serial = Serial(protocol)
+
+        # we are using "loop" port, so commands == results
+        commands = ["ciao_0", "ciao_1"]
+        stream = self._create_stream(commands, commands)
+        self.assertEqual(serial.send_command(commands, stream), commands)
+
+    def test_serial_port_error(self):
+        """ test a wrong serial port """
+        protocol = SerialProtocol(
+            "/dev/this_port_does_not_exist",
+            91200,
+            'odd',
+            1,
+            5,
+            1
+        )
+
+        serial = Serial(protocol)
+        with self.assertRaises(SerialConnectionError):
+            serial.send_command("something")
